2e4dfbef06e15de10b8f408635accd042ad1750a
Nicolas Gros <160612157+NicoGros@users.noreply.github.com>
2026-07-16 17:41:49 +0100

    Add files via upload

---
+++ MicroGPS/config.py
@@ -0,0 +1,13 @@
+BASE_DATA_DIRECTORY = ...
+
+IMAGE_EXTENSIONS = (
+    ".jpg",
+    ".jpeg",
+    ".png",
+)
+
+DEFAULT_EXPORT_FILE = "points_export.txt"
+
+CALIBRATION_PREFIX = "CP"
+
+MEASUREMENT_PREFIX = "MP"
