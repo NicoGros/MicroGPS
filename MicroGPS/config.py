99abfefe9ef6d41f1e32c5e98b5f9c95d070f57c
Nicolas Gros <160612157+NicoGros@users.noreply.github.com>
2026-07-22 13:58:43 +0100

    Update config.py

--- MicroGPS/config.py
+++ MicroGPS/config.py
@@ -1,3 +1,15 @@
+"""
+MicroGPS
+
+Interactive image registration and coordinate transformation tool for
+scientific surface analysis.
+
+Copyright (c) 2026 Nicolas Gros
+
+Licensed under the ? License.
+See the LICENSE file in the project root for details.
+"""
+
 BASE_DATA_DIRECTORY = ...
 
 IMAGE_EXTENSIONS = (
