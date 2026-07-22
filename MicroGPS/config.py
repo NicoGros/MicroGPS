2b513144969191bbfbb39ce92818d7d20a2a610c
Nicolas Gros <160612157+NicoGros@users.noreply.github.com>
2026-07-22 14:07:17 +0100

    Update config.py

--- MicroGPS/config.py
+++ MicroGPS/config.py
@@ -4,6 +4,11 @@ MicroGPS
 Interactive image registration and coordinate transformation tool for
 scientific surface analysis.
 
+Application configuration.
+
+Defines global constants, default settings, colours and user-adjustable
+parameters used throughout the application.
+
 Copyright (c) 2026 Nicolas Gros
 
 Licensed under the ? License.
@@ -12,14 +17,20 @@ See the LICENSE file in the project root for details.
 
 BASE_DATA_DIRECTORY = ...
 
+# Supported image file extensions
 IMAGE_EXTENSIONS = (
+    ".bmp",
+    ".gif",
     ".jpg",
     ".jpeg",
     ".png",
+    ".tif",
+    ".tiff",
 )
 
-DEFAULT_EXPORT_FILE = "points_export.txt"
+# Default export filename
+DEFAULT_EXPORT_FILE = "MicroGPS_coordinates.txt"
 
+# Point label prefixes
 CALIBRATION_PREFIX = "CP"
-
 MEASUREMENT_PREFIX = "MP"
